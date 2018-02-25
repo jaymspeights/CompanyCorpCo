5d3ddb49b96596180be37aae1a6c42ca1a5b9456
samendez <shanemendez223@gmail.com>
2018-02-25 02:23:34 -0600

    idk

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/req', logic);
+app.use('/', logic);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/logic.js
+++ routes/logic.js
@@ -4,43 +4,68 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 
-router.get('/reps/:lat/:lon', function(req, res) {
-  var lat = req.query.lat;
-  var lon = req.query.lon;
+router.get('/req/reps/:lat/:lon', function(req, res) {
+  var lat = req.params.lat;
+  var lon = req.params.lon;
   var uri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${GOOGLE_API_KEY}`;
-  request(uri, function (err, res, body) {
+  request(uri, function (err, response, body) {
     if (err) {
       res.render('error', {message: 'Oops! Something went wrong...', error: err});
       return;
     }
-    var address = body.results.formatted_address;
-    var uri = `https://www.googleapis.com/civicinfo/v2/representatives?address=${address}&roles=legislatorLowerBody&roles=legislatorUpperBody&includeOffices=true&key=${GOOGLE_API_KEY}`;
-    request(uri, function (err, res, body) {
-      if (err) {
+		body = JSON.parse(body);
+    var addresses = body.results;
+
+    getReps(addresses, function (error, sens, reps) {
+      if (error) {
         res.render('error', {message: 'Oops! Something went wrong...', error: err});
         return;
       }
-      var reps = [];
-      var sens = [];
-      var sens_i;
-      var reps_i;
-      for (var office of body.offices) {
-        if (office.name.match(/United States Senate.*/)) {
-          sens_i = office.officialIndices;
-        } else if (office.name.match(/United States House of Representatives.*/)) {
-          reps_i = office.officialIndices;
-        }
-      }
-      for (var i of sens_i) {
-        sens.push(body.officials[i]);
-      }
-      for (var i of rep_i) {
-        reps.push(body.officials[i]);
-      }
-      res.send({'sens': sens, 'reps':reps});
+      console.log(sens);
+      console.log(reps);
+      res.render('landing', {'sens': sens, 'reps':reps});
     });
   });
 
 });
+
+function getReps(addresses, cb) {
+  if (addresses.length == 0) {
+    cb({status:"We couldn't seem to find you.", stack: "Try entering the address in manually."});
+    return;
+  }
+  var address = addresses.shift().formatted_address;
+  address = encodeURIComponent(address);
+  var uri = `https://www.googleapis.com/civicinfo/v2/representatives?address=${address}&roles=legislatorLowerBody&roles=legislatorUpperBody&includeOffices=true&key=${GOOGLE_API_KEY}`;
+  request(uri, function (err, response, body) {
+    if (err) {
+      getReps(addresses, cb);
+      return;
+    }
+    body = JSON.parse(body);
+    if (body.error) {
+      getReps(addresses, cb);
+      return;
+    }
+    var reps = [];
+    var sens = [];
+    var sens_i;
+    var reps_i;
+    for (var office of body.offices) {
+      if (office.name.match(/United States Senate.*/)) {
+        sens_i = office.officialIndices;
+      } else if (office.name.match(/United States House of Representatives.*/)) {
+        reps_i = office.officialIndices;
+      }
+    }
+    for (var i of sens_i) {
+      sens.push(body.officials[i]);
+    }
+    for (var i of reps_i) {
+      reps.push(body.officials[i]);
+    }
+    cb(undefined, sens, reps);
+  });
+}
 
 module.exports = router;
