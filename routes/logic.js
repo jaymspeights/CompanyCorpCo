0913d2a0a173cecc3b96d0c684e6b32de0c9c55a
jay speights <jaymspeights@gmail.com>
2018-02-25 06:28:08 -0600

    connects to database

--- routes/logic.js
+++ routes/logic.js
@@ -1,8 +1,21 @@
 var GOOGLE_API_KEY = 'AIzaSyC8Bkp_SWIgz2PRCyURtEXiXtZ2v9KcplQ';
 
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
 var request = require('request');
 var express = require('express');
+var db;
 var router = express.Router();
+
+var uri = fs.readFileSync('./db_uri', 'utf8').replace('\n', '');
+MongoClient.connect(uri, function (err, _db) {
+  if (err) {
+    console.log(err);
+    throw new Error("Ya fucked up");
+  }
+  db = _db.db('KYR');
+});
 
 var current_user_id;
 function getCurrentUserId() {
@@ -11,19 +24,36 @@ function getCurrentUserId() {
   });
 }
 
-function getNewId() {
-  //talk to database
-  //replace this data spoof
-  return 0;
+function getNewId(cb) {
+  db.collection('users').count(function (err, count) {
+    if (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+    cb(count)
+  });
 }
 
 function getMostRecentBill(cb) {
   cb();
 }
 
+function getUserById (id, cb) {
+  db.collection('users').findOne({'_id':id}, function (err, user) {
+    if (err)
+      throw err;
+    cb(user)
+  });
+}
+
+
 router.get('/req/id', function (req, res) {
   current_user_id += 1;
-  res.send({id:current_user_id});
+  db.collection('users').insertOne({'_id': current_user_id}, function (err, response) {
+    if (err)
+      throw err;
+    res.send({id:current_user_id});
+  });
 });
 
 router.get('/req/bills/new', function (req, res) {
@@ -37,28 +67,43 @@ router.get('/req/bills/new', function (req, res) {
 })
 
 router.get('/req/reps', function(req, res) {
-  var loc = req.query.latlng;
-  var uri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${loc}&key=${GOOGLE_API_KEY}`;
-  request(uri, function (err, response, body) {
-    if (err) {
-      res.render('error', {message: 'Oops! Something went wrong...', error: err});
+  var id = req.query.id;
+  getUserById(id, function (user) {
+    console.log(user)
+    if (user == undefined) {
+      res.render('error', {message: 'Oops! Something went wrong...', error: {status:"You don't have a user id yet.", stack: "Stop fucking around with our api."}});
       return;
-    }
-		body = JSON.parse(body);
-    var addresses = body.results;
+    } else if (user.senators == undefined || user.representatives == undefined) {
+      var loc = req.query.latlng;
+      var uri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${loc}&key=${GOOGLE_API_KEY}`;
+      request(uri, function (err, response, body) {
+        if (err) {
+          res.render('error', {message: 'Oops! Something went wrong...', error: err});
+          return;
+        }
+    		body = JSON.parse(body);
+        var addresses = body.results;
 
-    getReps(addresses, function (error, sens, reps) {
-      if (error) {
-        res.render('error', {message: 'Oops! Something went wrong...', error: err});
-        return;
-      }
-      res.render('landing', {'sens': sens, 'reps':reps});
+        getRepsFromApi(user, addresses, function (error, sens, reps) {
+          if (error) {
+            res.render('error', {message: 'Oops! Something went wrong...', error: err});
+            return;
+          }
+          res.render('landing', {'sens': sens, 'reps':reps});
+        });
+      });
+    }
+    db.collection('senators').findAll({'id':{'$in' : user.senators}}, function (err, sens) {
+      if (err) throw err;
+      db.collection('representatives').findAll({'id':{'$in' : user.representatives}}, function (err, reps) {
+        if (err) throw err;
+        res.render('landing', {'sens': sens, 'reps':reps});
+      });
     });
   });
-
 });
 
-function getReps(addresses, cb) {
+function getRepsFromApi(user, addresses, cb) {
   if (addresses.length == 0) {
     cb({status:"We couldn't seem to find you.", stack: "Try entering the address in manually."});
     return;
@@ -68,12 +113,12 @@ function getReps(addresses, cb) {
   var uri = `https://www.googleapis.com/civicinfo/v2/representatives?address=${address}&roles=legislatorLowerBody&roles=legislatorUpperBody&includeOffices=true&key=${GOOGLE_API_KEY}`;
   request(uri, function (err, response, body) {
     if (err) {
-      getReps(addresses, cb);
+      getRepsFromApi(user, addresses, cb);
       return;
     }
     body = JSON.parse(body);
     if (body.error) {
-      getReps(addresses, cb);
+      getRepsFromApi(user, addresses, cb);
       return;
     }
     var reps = [];
@@ -87,13 +132,20 @@ function getReps(addresses, cb) {
         reps_i = office.officialIndices;
       }
     }
+    //user.senators = [];
     for (var i of sens_i) {
       sens.push(body.officials[i]);
+      //user.senators.push(body.officials[i].id);
     }
+    //user.representatives = [];
     for (var i of reps_i) {
       reps.push(body.officials[i]);
+      //user.representatives.push(body.officials[i].id);
     }
-    cb(undefined, sens, reps);
+    db.collection('users').updateOne({"_id":user._id}, {"$set":user}, {"upsert":true}, function (err, res) {
+      if (err) throw err;
+      cb(undefined, sens, reps);
+    });
   });
 }
 
